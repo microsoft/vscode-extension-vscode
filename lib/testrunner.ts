ee58d91f102a59bb6d452bcba0e11eacb85062c5
Benjamin Pasero <benjpas@microsoft.com>
2016-01-31 16:36:53 +0100

    fix warnings

--- lib/testrunner.ts
+++ lib/testrunner.ts
@@ -16,48 +16,48 @@ import * as glob from 'glob';
 // Since we are not running in a tty environment, we just implementt he method statically
 var tty = require('tty');
 if (!tty.getWindowSize) {
-	tty.getWindowSize = function() { return [80, 75]; };
+    tty.getWindowSize = function() { return [80, 75]; };
 }
 
 import Mocha = require('mocha');
 
 
 let mocha = new Mocha({
-	ui: 'tdd',
-	useColors: true
+    ui: 'tdd',
+    useColors: true
 });
 
 export function configure(opts: MochaSetupOptions): void {
-	mocha = new Mocha(opts);
+    mocha = new Mocha(opts);
 }
 
 export function run(testsRoot: string, clb: (error, failures?: number) => void): void {
-	
-	// Enable source map support
-	require('source-map-support').install();
-	
-	// Glob test files
-	glob('**/**.test.js', { cwd: testsRoot }, (error, files) => {
-		if (error) {
-			return clb(error);
-		}
 
-		try {
-			
-			// Fill into Mocha
-			files.forEach(f => mocha.addFile(paths.join(testsRoot, f)));
-	
-			// Run the tests
-			let failures = 0;
-			mocha.run()
-				.on('fail', function(test, err) {
-					failures++;
-				})
-				.on('end', function() {
-					clb(null, failures);
-				});
-		} catch (error) {
-			return clb(error);
-		}
-	});
+    // Enable source map support
+    require('source-map-support').install();
+
+    // Glob test files
+    glob('**/**.test.js', { cwd: testsRoot }, (error, files) => {
+        if (error) {
+            return clb(error);
+        }
+
+        try {
+
+            // Fill into Mocha
+            files.forEach(f => mocha.addFile(paths.join(testsRoot, f)));
+
+            // Run the tests
+            let failures = 0;
+            mocha.run()
+                .on('fail', function(test, err) {
+                    failures++;
+                })
+                .on('end', function() {
+                    clb(null, failures);
+                });
+        } catch (error) {
+            return clb(error);
+        }
+    });
 }
