9d52ad14ca6828c23163cf1afe72aaab33bbcc2d
Benjamin Pasero <benjpas@microsoft.com>
2016-04-23 12:30:54 +0200

    delete unused file

--- typings/index-js.d.ts
+++
@@ -1,7 +0,0 @@
-/// <reference path="./core.d.ts" />
-/// <reference path="./global.d.ts" />
-/// <reference path="./es6.d.ts" />
-/// <reference path="./intl.d.ts" />
-/// <reference path="./node.d.ts" />
-/// <reference path="./mocha.d.ts" />
-/// <reference path="./vscode.raw.d.ts" />
